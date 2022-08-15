bef448dd1753466eaff71e5e2c51ca972ed954a2
Bruno DaSilva <hi.dasilvab@gmail.com>
2022-08-14 19:24:35 -0500

    imported mongoose and DB connection to server

--- api/server.js
+++ api/server.js
@@ -2,14 +2,18 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
-const { logger } = require("./middleware/logger");
+const { logger, logEvents } = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
+const connectDB = require("./config/dbConnection");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3500;
 
 console.log(process.env.NODE_ENV);
+
+connectDB();
 
 // => Middleware
 
@@ -37,6 +41,18 @@ app.all("*", (req, res) => {
 app.use(errorHandler);
 
 // => Listening to App on Specified Port
-app.listen(PORT, () =>
-  console.log(`Server running on: http://localhost:${PORT}/`)
-);
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+  app.listen(PORT, () =>
+    console.log(`Server running on: http://localhost:${PORT}/`)
+  );
+});
+
+// => Listening for Connection Error
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoErrLog.log"
+  );
+});
